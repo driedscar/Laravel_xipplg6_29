f7fbafa2917cfca09f59838cee560c04b39f6c41
Neyla Alma <neylalma047@gmail.com>
2025-10-29 15:30:11 +0700

    Add Feature Create Student

--- app/Http/Controllers/Admin/StudentController.php
+++ app/Http/Controllers/Admin/StudentController.php
@@ -23,7 +23,7 @@ class StudentController extends Controller
      */
     public function create()
     {
-        //
+        return view('admin.student.create');
     }
 
     /**
@@ -31,7 +31,15 @@ class StudentController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $request->validate([
+            'nis' => 'required|unique:students',
+            'nama_lengkap' => 'required',
+            'jenis_kelamin' => 'required',
+            'nisn' => 'required|unique:students',
+        ]);
+
+        Student::create($request->all());
+        return redirect()->route('admin.students.index')->with('success', 'Data berhasil disimpan!');
     }
 
     /**
